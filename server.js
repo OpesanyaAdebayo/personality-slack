f86213a440d5f1e5903b2620c1fbae16030a025c
Opesanya Adebayo <bayoopesanya@gmail.com>
2018-03-27 03:13:55 +0100

    integrated slack events api

--- server.js
+++ server.js
@@ -4,7 +4,23 @@ const request = require('request');
 var bodyParser = require('body-parser');
 const app = express();
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
-
+app.use(bodyParser.json());
 app.listen(3200);
 
 console.log("listening");
+
+// Initialize using verification token from environment variables
+const createSlackEventAdapter = require('@slack/events-api').createSlackEventAdapter;
+const slackEvents = createSlackEventAdapter(process.env.SLACK_VERIFICATION_TOKEN);
+
+// Mount the event handler on a route
+// NOTE: you must mount to a path that matches the Request URL that was configured earlier
+app.use('/slack/events', slackEvents.expressMiddleware());
+
+// Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
+slackEvents.on('message', (event)=> {
+  console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
+});
+
+// Handle errors (see `errorCodes` export)
+slackEvents.on('error', console.error);
